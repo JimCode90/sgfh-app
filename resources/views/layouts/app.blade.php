24eea74776d5157798fc4557f41e5b3edd9b5708
Subzer09 <jimmypaytan10@gmail.com>
2022-09-05 21:09:51 -0500

    modificando login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,14 +47,14 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('INICIAR SESIÓN') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
+{{--                                </li>--}}
                             @endif
                         @else
                             <li class="nav-item dropdown">
@@ -66,7 +66,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('CERRAR SESIÓN') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,9 +26,9 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline" style="color: white">Home</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline" style="color: white">DASHBOARD</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline" style="color: white">Login</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline" style="color: white">INICIAR SESION</a>
                     @endif
                 </div>
             @endif
